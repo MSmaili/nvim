7c76e85b109b46a7fdd054ef02714cbad8178358
Muhamet Smaili <muhamet5@msn.com>
2025-10-25 09:01:58 +0300

    remove neocodium in favor of sidekick

--- lua/custom/plugins/code.lua
+++ lua/custom/plugins/code.lua
@@ -1,43 +1,5 @@
 return {
 	{ "tpope/vim-repeat", keys = { { "." }, { ";" } } },
-	--codeiumn
-	{
-		"monkoose/neocodeium",
-		-- event = "VeryLazy",
-		lazy = true,
-		opts = {
-			manual = true,
-			-- filter = function()
-			-- 	local cmp = require("blink-cmp")
-			-- 	return not cmp.is_visible()
-			-- end,
-			debounce = true,
-			server = {},
-		},
-		config = function(_, opts)
-			local neocodeium = require("neocodeium")
-			neocodeium.setup(opts)
-			--
-			vim.keymap.set("i", "<a-y>", neocodeium.accept)
-			vim.keymap.set("i", "<a-[>", function()
-				neocodeium.cycle(-1)
-			end)
-			vim.keymap.set("i", "<a-]>", function()
-				neocodeium.cycle(1)
-			end)
-			--
-			-- vim.keymap.set("i", "<a-c>", function()
-			-- 	neocodeium.cycle_or_complete()
-			-- end)
-			--
-			-- vim.keymap.set("i", "<esc>", "<c-c>")
-			-- local cmp = require("cmp")
-			-- cmp.event:on("menu_opened", function()
-			-- 	neocodeium.clear()
-			-- end)
-		end,
-	},
-
 	-- opposite and increment/decrement
 	{
 		"nat-418/boole.nvim",
---
+++ lua/custom/plugins/sidekick-nvim.lua
@@ -0,0 +1,70 @@
+return {
+	"folke/sidekick.nvim",
+	opts = {
+		nes = { enabled = false },
+		cli = {
+			mux = {
+				backend = "tmux",
+				enabled = true,
+			},
+		},
+		prompts = {
+			picker = "fzf-lua",
+		},
+	},
+	keys = {
+		{
+			"<leader>aa",
+			function()
+				require("sidekick.cli").toggle({ name = "amazon_q", focus = true })
+				-- require("sidekick.cli").toggle()
+			end,
+			desc = "Sidekick Toggle CLI (amazon_q)",
+		},
+		{
+			"<leader>ah",
+			function()
+				require("sidekick.cli").hide()
+			end,
+			desc = "Hide the cli Session",
+		},
+		{
+			"<leader>ad",
+			function()
+				require("sidekick.cli").close()
+			end,
+			desc = "Detach a CLI Session",
+		},
+		{
+			"<leader>at",
+			function()
+				require("sidekick.cli").send({ msg = "{this}" })
+			end,
+			mode = { "x", "n" },
+			desc = "Send This",
+		},
+		{
+			"<leader>af",
+			function()
+				require("sidekick.cli").send({ msg = "{file}" })
+			end,
+			desc = "Send File",
+		},
+		{
+			"<leader>av",
+			function()
+				require("sidekick.cli").send({ msg = "{selection}" })
+			end,
+			mode = { "x" },
+			desc = "Send Visual Selection",
+		},
+		{
+			"<leader>ap",
+			function()
+				require("sidekick.cli").prompt()
+			end,
+			mode = { "n", "x" },
+			desc = "Sidekick Select Prompt",
+		},
+	},
+}
